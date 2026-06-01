eb02886d39aaad2a25a53fc4c045945e1edce28b
motoroboto <robert.a.cullen@gmail.com>
2026-06-01 17:14:58 -0400

    I had missed a comma

--- src/js/data/2025-11-30.js
+++ src/js/data/2025-11-30.js
@@ -1526,13 +1526,14 @@ dataSet[dataSetVersion].songData = [
       bonus: [],
     },
   },
-  {name: "Heaven's In Here",
+  {
+    name: "Heaven's In Here",
     img: "tm.jpg",
-  opts: {
-    albums: [],
-    bonus: ["TM"]
+    opts: {
+      albums: [],
+      bonus: ["TM"],
+    },
   },
-}
   {
     name: "The Wedding",
     img: "btwn.jpg",
